c68032459b2f41fd281c78cb31ca38c04e2f37e4
Landy Simpson <landy@theredpin.com>
2016-07-06 17:03:09 -0400

    fix the weird dropdown menu problem

---
+++ public/javascripts/dropmenu.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+	isDropMenu();
+		
+	$(window).resize(function(){
+		isDropMenu();		
+	});
+})
+
+/*
+	Decides if the dropdown menu should be shown or hidden
+*/
+function isDropMenu() {
+	winSize = $(window).width();
+	docSize = $(document).width();
+	console.log(winSize, docSize);
+	if (docSize > winSize) {
+		console.log('SHOW!');
+		showDropMenu();
+	} else {
+		console.log('HIDE!');
+		hideDropMenu();
+	}
+}
+
+function showDropMenu() {
+	if (!$(".dropdown-toggle").length) {
+
+		// Add a dropdown button to the mobile menu
+		$("ul.mobile-nav").before("<a class='dropdown-toggle' data-toggle='dropdown' href='#' role='button' aria-haspopup='true' aria-expanded='false'> Collapsed <span class='caret'></span> </a>");
+
+		// add the dropdown menu class to the mobile menu
+		$("ul.mobile-nav").toggleClass("dropdown-menu");
+
+		// hide the other to links
+		$(".navbar-right").css("display", "none");
+	}
+}
+
+function hideDropMenu() {
+	$(".dropdown-toggle").remove();
+	$("ul.mobile-nav").removeClass("dropdown-menu");
+	$('.navbar-right').css("display", "inherit");
+}
